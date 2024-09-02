915e923f874fe3b64bdb2cf52d59f1e8b5a21134
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2024-09-02 15:18:56 +0200

    SetPassword.js

---
+++ src/components/SetPassword.js
@@ -0,0 +1,15 @@
+import { useRecoilState } from "recoil"
+import loaderAtom from "../recoil/loaderAtom"
+import postSetPasswordAction from '../actions/postSetPasswordAction'
+
+export default function() {
+    const [loader, setLoader] = useRecoilState(loaderAtom)
+
+    return <>
+        <input type="password" placeholder="Heslo" />
+        <input type="password" placeholder="Potvrdit heslo" />
+        <SendButton label="Nastavit heslo"
+            loader={loader}
+            onClick={() => postSetPasswordAction(setLoader)} />
+    </>
+}
