bc286a64968b1c761bd4d8b913cec4672b7a0e7a
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2024-09-02 15:03:41 +0200

    SignUp.js

---
+++ src/components/SignUp.js
@@ -0,0 +1,17 @@
+import { useRecoilState } from "recoil"
+import signUpLoaderAtom from '../recoil/loaderAtom'
+import SendButton from './SendButton'
+import postSignUpAction from "../actions/postSignUpAction"
+
+export default function() {
+    const [loader, setLoader] = useRecoilState(signUpLoaderAtom)
+
+    return <>
+        <input type="text" placeholder="E-mail" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm password" />
+        <SendButton label="Zaregistrovat se"
+            loader={loader}
+            onClick={() => postSignUpAction(setLoader)} />
+    </>
+}
