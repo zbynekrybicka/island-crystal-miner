2d7000da2da9dd04a655a8e1f7f305ec2636c08b
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2024-09-02 14:07:28 +0200

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,9 @@
-import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil'
-
-import helloAction from '../actions/helloAction'
-import incrementAction from '../actions/incrementAction'
-
-import helloAtom from '../recoil/helloAtom'
-import loaderAtom from '../recoil/loaderAtom'
-import testValueAtom from '../recoil/testValueAtom'
+import { useRecoilValue } from 'recoil'
+import isLoggedInSelector from '../recoil/isLoggedInSelector'
 
 function App() {
-  const [ loader, setLoader ] = useRecoilState(loaderAtom)
-  const testValueState = useRecoilState(testValueAtom)
-
-  const setHello = useSetRecoilState(helloAtom)
-
-  const helloValue = useRecoilValue(helloAtom)
-  const [testValue] = testValueState
-
-  return (
-    <div className="App">
-        {loader && <div className="loader">...</div>}
-        {testValue}
-        <button onClick={() => incrementAction(testValueState)}>Increment</button>
-        <hr />
-        {helloValue}
-        <button onClick={() =>helloAction(setLoader, setHello)}>Hello</button>
-    </div>
-  );
+  const isLoggedIn = useRecoilValue(isLoggedInSelector)
+  return isLoggedIn ? <Game /> : <Login />
 }
 
 export default App;
