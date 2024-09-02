8a942a9866a6e811f512ba7ae9a2b6316d96c960
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2024-09-02 14:48:34 +0200

    EnergySources.js

---
+++ src/components/EnergySources.js
@@ -0,0 +1,12 @@
+import { useRecoilValue } from "recoil"
+
+import miningEnergySourcesSelector from './recoil/miningEnergySourcesSelector'
+import chargeBatteryAction from '../actions/chargeBatteryAction'
+
+export default function() {
+    const miningEnergySources = useRecoilValue(miningEnergySourcesSelector)
+
+    return <>
+        <button onClick={() => chargeBatteryAction()}></button>
+    </>
+}
