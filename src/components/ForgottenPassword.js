682b321a5dcf3fe7c3b81f046475ba02ccbe655d
Zbynek Rybicka <zbynek.rybicka@gmail.com>
2024-09-02 15:10:18 +0200

    ForgottenPassword.js

---
+++ src/components/ForgottenPassword.js
@@ -0,0 +1,13 @@
+import loaderAtom from "../recoil/loaderAtom"
+import postClearPasswordAction from '../postClearPasswordAction'
+
+export default function() {
+    const [loader, setLoader] = useRecoilState(loaderAtom)
+
+    return <>
+        <input type="text" placeholder="E-mail" />
+        <SendButton label="Obnovit heslo"
+            loader={loader}
+            onClick={() => postClearPasswordAction(setLoader)} />
+    </>
+}
